0502fad556d1a41b66c41ea35b592c14ca996c9a
Ana Paula Viana <130803295+cc23504@users.noreply.github.com>
2025-06-02 19:12:22 -0300

    att

--- MiniAPI_23504/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ MiniAPI_23504/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5ec94a032ce4e19bb557ac9a06336d3cab7262")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
